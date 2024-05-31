b5ac3195b7bc87ad13365be1b045306c02d8cd34
elijah -kimani <elijahkimani1293@gmail.com>
2024-05-31 10:51:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// {
+    
+//       var button = document.getElementById("button");
+
+//       button.onclick = function() {
+//           document.body.style.backgroundColor = "red";
+//       };
+
+    
+//     }
+
+//function dark(){
+    //document.body.style.backgroundColor="white";
+    
+//}
+//function dark() {
+    //var facebookDev = document.getElementById("facebookDev");
+
+    //facebookDev.style.backgroundColor="white";
+const lightMode=()=>{
+    document.body.classList.toggle('lightMode')
+}
